c39efffea0affe1fec87a9a3609875631c7b4680
Aryan Shah <aryankshah15@gmail.com>
2026-06-20 21:45:06 +0530

    Add files via upload

--- gemini_helper.py
+++ gemini_helper.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
-from config import GEMINI_API_KEY
+import streamlit as st
 
-genai.configure(api_key=GEMINI_API_KEY)
+genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
